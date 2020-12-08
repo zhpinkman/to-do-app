06cc02af671b007a9775a63325058e169b9f057c
zhivar <zhivarsourati@gmail.com>
2020-12-08 15:08:33 +0330

    Add all the modules, services, and controllers for books, genre, and user

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,12 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { HelloModule } from './hello/hello.module';
+import { BooksModule } from './books/books.module';
+import { UserModule } from './user/user.module';
+import { GenreModule } from './genre/genre.module';
 
 @Module({
-  imports: [HelloModule],
+  imports: [HelloModule, BooksModule, UserModule, GenreModule],
   controllers: [AppController],
   providers: [AppService],
 })
