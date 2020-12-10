8bb2d87fbb6888efd4053b20d14af198a501c059
zhivar <zhivarsourati@gmail.com>
2020-12-10 11:29:08 +0330

    Add controllers of the label and category

---
+++ src/label/label.controller.ts
@@ -0,0 +1,26 @@
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { ApiResponse } from '@nestjs/swagger/dist/decorators';
+import CreateLabelDto from 'src/todo/dto/create-label.dto';
+import { LabelService } from './label.service';
+
+@Controller('label')
+export default class labelController {
+  constructor(private readonly labelServices: LabelService) {}
+  @ApiResponse({
+    status: 201, 
+    description: 'returns the created label'
+  })
+  @Post('post')
+  postlabel( @Body() label: CreateLabelDto) {
+    return this.labelServices.insert(label);
+  }
+
+  @ApiResponse({
+    status: 200, 
+    description: 'returns all the labels'
+  })
+  @Get()
+  getAll() {
+    return this.labelServices.getAlllabel();
+  }
+}
