1f72a918d64e623182a6d4ad844f7e396c38504e
zhivar <zhivarsourati@gmail.com>
2020-12-13 22:46:33 +0330

    Update the name of the swagger document

--- src/main.ts
+++ src/main.ts
@@ -7,10 +7,10 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   const options = new DocumentBuilder()
-  .setTitle('Cats example')
-  .setDescription('The cats API description')
+  .setTitle('todo example')
+  .setDescription('The todo API description')
   .setVersion('1.0')
-  .addTag('cats')
+  .addTag('todo')
   .build();
   const document = SwaggerModule.createDocument(app, options);
   SwaggerModule.setup('/', app, document);
